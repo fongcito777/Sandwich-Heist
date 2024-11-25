5c1c3e5b41cc6cab9d3d3c705b93ddb3ec5ac22c
Raquel Magdalena Ochoa Martínez <0235324@up.edu.mx>
2024-11-25 12:37:56 -0600

    solo espacios para prueba

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,6 +17,9 @@ public class GameManager : MonoBehaviour
         _mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
     
+
+
+
     void Update()
     {
     }
